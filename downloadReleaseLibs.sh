0946e27913d01eec2c107f4a36367222d8ebcfab
DanFu <felydefely@gmail.com>
2020-12-11 13:31:59 +0800

    update demo repo

--- downloadReleaseLibs.sh
+++ downloadReleaseLibs.sh
@@ -1,6 +1,6 @@
 file_name=depedencies.zip
 
-curl -O https://celer.oss-cn-shanghai.aliyuncs.com/iOSSdkArchive/v2_15_2/$file_name
+curl -O https://celer.oss-cn-shanghai.aliyuncs.com/iOSSdkArchive/v2_17_0/$file_name
 
 rm -r depedencies
 
